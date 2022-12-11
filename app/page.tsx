bdbb78c3112e823aa380ae87d12d09f5641490fd
Antonio J <antonio.jhun@digistorm.com>
2022-12-11 20:58:09 +1000

    load news articles from Mediastack api, set article layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { categories } from '../constants';
+import fetchNews from '../lib/fetchNews';
+import NewsList from './NewsList';
+import response from '../__mocks__/response.json';
 
-function Homepage() {
-  return <div>Homepage</div>;
+async function Homepage() {
+  // fetch the news data
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(',')));
+
+  console.log(news);
+
+  return (
+    <div>
+      {/* NewsList news */}
+      <NewsList news={news} />
+    </div>
+  );
 }
 
 export default Homepage;

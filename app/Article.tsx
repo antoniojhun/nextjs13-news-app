da66879269dd1bd0b09d14df8ad643556431310e
Antonio J <antonio.jhun@digistorm.com>
2022-12-19 19:33:53 +1000

    Change clamp to 3 lines

--- app/Article.tsx
+++ app/Article.tsx
@@ -16,7 +16,7 @@ function Article({ article }: Props) {
         <div className="flex-1 flex flex-col p-5">
           <h2 className="font-bold font-serif">{article.title}</h2>
           <section className="mt-2 flex-1">
-            <p className="text-xs line-clamp-2">{article.description}</p>
+            <p className="text-xs line-clamp-3">{article.description}</p>
           </section>
           <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
             <p className="text-gray-500 mt-2">{article.source} -</p>
